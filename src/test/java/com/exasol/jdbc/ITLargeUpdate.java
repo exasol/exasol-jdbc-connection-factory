490982fe197cfea62dd7a4b513098d3a09ef739b
Stefan Reich <stefan.reich@exasol.com>
2023-06-27 08:43:31 +0200

    - removed unneeded parts of the new test

--- src/test/java/com/exasol/jdbc/ITLargeUpdate.java
+++ src/test/java/com/exasol/jdbc/ITLargeUpdate.java
@@ -17,7 +17,7 @@ class ITLargeUpdate {
     protected final ExasolContainer<? extends ExasolContainer<?>> database = new ExasolContainer<>().withRequiredServices().withReuse(true);
 
     @BeforeAll
-    void startUp() {
+    void startUp() throws SQLException {
         database.start();
 
         try (
@@ -28,51 +28,38 @@ class ITLargeUpdate {
                     "d1 = decimal(mul1)\n" +
                     "d2 = decimal(mul2)\n" +
                     "return { rows_affected = d1*d2 }");
-        } catch (SQLException e) {
-            fail(e);
         }
     }
 
     @AfterAll
     void cleanUp()
     {
-        try (
-                Connection c = database.createConnection()
-        ) {
-            c.createStatement().executeUpdate("drop schema row_counter cascade");
-        } catch (SQLException e) {
-            fail(e);
-        }
         database.stop();
     }
 
     // Make sure that executeUpdate() returns correct rowcounts within reasonable bounds
     @ParameterizedTest
     @ValueSource(longs = {1, 10, 100, 1000, 10000, 100000, 1000000})
-    void testExecuteUpdate(long row_root) {
+    void testExecuteUpdate(long row_root) throws SQLException {
         try (
                 ManagedConnection mc = new ManagedConnection(database.createConnection())
         ) {
             assertEquals(row_root*row_root, mc.executeUpdate(String.format(
                     "execute script row_counter.count_rows(%d, %d)", row_root, row_root
                 )));
-        } catch (SQLException e) {
-            fail(e);
         }
     }
 
     // Make sure that executeUpdatePrepared() returns correct rowcounts within reasonable bounds
     @ParameterizedTest
     @ValueSource(longs = {1, 10, 100, 1000, 10000, 100000, 1000000})
-    void testExecuteUpdatePrepared(long row_root) {
+    void testExecuteUpdatePrepared(long row_root) throws SQLException {
         try (
                 ManagedConnection mc = new ManagedConnection(database.createConnection())
         ) {
             assertEquals(row_root*row_root, mc.executeUpdatePrepared(String.format(
                     "execute script row_counter.count_rows(%d, %d)", row_root, row_root
             ), new Object[]{}));
-        } catch (SQLException e) {
-            fail(e);
         }
     }
 }
